30c46af3f752ff8be7f3a2ae793c8404d48cf1a6
pipinosuke <ys.pipinosuke@gmail.com>
2017-11-30 18:08:08 +0900

    修正

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::GroupsController < ApplicationController
   def index
-    #@users = User.where("addr1 = ?", user_params[:addr1])
     @groups = Group.all
-    #where("name = ?", group_params[:name])
     render json: @groups
   end
 
