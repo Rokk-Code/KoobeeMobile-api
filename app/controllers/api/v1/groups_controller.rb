60d09b093c9fb217b7f01a6d668b4f3bde0d1651
pipinosuke <ys.pipinosuke@gmail.com>
2017-12-05 22:57:10 +0900

    render忘れ

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::GroupsController < ApplicationController
   def index
-      @groups = Group.all.shuffle.take(params[:limit].to_i)
-      render json: @groups
+    @groups = Group.all.shuffle.take(params[:limit].to_i)
+    render json: @groups
   end
 
   def search
     @groups = Group.search_keywords(params[:keywords])
+    render json: @groups
   end
 
   private
