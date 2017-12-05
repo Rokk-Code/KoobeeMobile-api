c6929490cec82e23fde7421edd2a182a48544381
pipinosuke <ys.pipinosuke@gmail.com>
2017-12-05 21:20:47 +0900

    修正

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::GroupsController < ApplicationController
   def index
     if params[:keywords] == nil || ""
-      @groups = Group.all.shuffle.limit(params[:limit])
+      @groups = Group.all.shuffle.take(params[:limit].to_i)
     else
       @groups = Group.search_keywords(params[:keywords])
     end
